bccce88b4a050a2602fcf83e93955622b4165d6f
nicoloGiaccone <giacco9722@outlook.it>
2019-12-06 09:49:01 +0100

    iniziallizzata repository

---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Nicolò Giaccone 1171208
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
